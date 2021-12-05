30f1a1755ff55700a5cc4485f7f1a41653d73061
Nathan Smith <nathan.smith@paytron.com.au>
2021-12-05 22:50:44 +1300

    - Continued with getting playlist data - Got something working with listing playlists

--- next.config.js
+++ next.config.js
@@ -1,9 +1,13 @@
 module.exports = {
-  webpack: (config) => {
+  webpack: (config, { isServer }) => {
     config.module.rules.push({
       test: /\.md$/,
       loader: 'frontmatter-markdown-loader',
     });
+    // Fixes npm packages that depend on `fs` module
+    if (!isServer) {
+      config.resolve.fallback.fs = false
+    }
     return config;
   },
 };
--- pages/playlists/index.tsx
+++ pages/playlists/index.tsx
@@ -1,7 +1,7 @@
 import { GetStaticProps } from 'next'
 import { Playlist } from 'interfaces'
 import Layout from 'components/Layout'
-import List from 'components/Playlists'
+import Link from 'next/link'
 
 type Props = {
   items: Playlist[]
@@ -10,12 +10,36 @@ type Props = {
 const Playlists = ({ items }: Props) => (
   <Layout title="Playlists | Community">
     <h1>Playlists</h1>
-    {items && <List items={items} />}
+    <ul>
+      {items.map((item, index) => (
+        <li key={index}>
+          <Link href="/playlists/[item]" as={`/playlists/${item}`}>
+            <a>
+              {item}
+            </a>
+          </Link>
+        </li>
+      ))}
+    </ul>
   </Layout>
 )
 
 export const getStaticProps: GetStaticProps = async () => {
-  return { props: { item: [] } }
+  const items = (context => {
+    const keys = context.keys()
+    console.log(keys)
+    const data = keys.map((key) => {
+      console.log(key)
+      const slug = key
+        .replace(/^.*[\\\/]/, '')
+        .split('.')
+        .slice(0, -1)
+        .join('.')
+      return slug
+    })
+    return data
+  })(require.context('content/playlists', false, /\.md$/))
+  console.log(items)
+  return { props: { items } }
 };
-
 export default Playlists
