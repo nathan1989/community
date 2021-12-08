c16d708997e44cf65ddbac7841774aa9bcdd577d
Nathan Smith <nathan.smith@paytron.com.au>
2021-12-08 22:48:21 +1300

    - Fixed cms config issue - Added markdown parser for playlist page

--- components/PlaylistSection.tsx
+++ components/PlaylistSection.tsx
@@ -1,17 +1,19 @@
-
+import { NextPage } from 'next'
 import { PlaylistProps } from 'interfaces'
+import Markdown from 'markdown-to-jsx'
 import moment from 'moment'
 
 type Props = {
   item: PlaylistProps
 }
 
-const PlaylistSection = ({ item }: Props) => (
+const PlaylistSection: NextPage<Props> = ({ item }) => (
     <div>
         <h1>{item.title}</h1>
         <p>{moment(item.date).format('Do MMM YYYY [at] h:mm A z')}</p>
         <p>{item.description}</p>
-        <div dangerouslySetInnerHTML={{ __html: item.playlist }} />
+        <Markdown>{item.playlist}</Markdown>
+        <Markdown>{item.lyrics}</Markdown>
     </div>
 )
 
--- pages/admin.tsx
+++ pages/admin.tsx
@@ -1,12 +1,13 @@
 import dynamic from 'next/dynamic';
 import PlaylistPreview from 'components/admin/PlaylistPreview';
+import config from 'cms/config';
 
 const AdminWithNoSSR = dynamic(
   () =>
     import("netlify-cms-app").then((CMS: any) => {
       CMS.registerPreviewStyle("/admin/main.css");
       CMS.registerPreviewTemplate("playlists", PlaylistPreview)
-      CMS.init();
+      CMS.init({ config });
     }) as any,
   { ssr: false }
 );
