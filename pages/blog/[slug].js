ab71cd2b876e544d84bac534cd2ef213495a394f
Nathan Smith <nathan.smith@paytron.com.au>
2022-03-30 22:34:25 +1300

    single blog page

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -17,7 +17,7 @@ export default function Blog({ frontmatter, markdown }) {
 
 export async function getStaticProps({ params: { slug } }) {
   const fileContent = matter(
-    fs.readFileSync(`./content/playlists/${slug}.md`, "utf8")
+    fs.readFileSync(`./content/blog/${slug}.md`, "utf8")
   );
   let frontmatter = fileContent.data;
   const markdown = fileContent.content;
@@ -28,7 +28,7 @@ export async function getStaticProps({ params: { slug } }) {
 }
 
 export async function getStaticPaths() {
-  const filesInProjects = fs.readdirSync("./content/playlists");
+  const filesInProjects = fs.readdirSync("./content/blog");
 
   // Getting the filenames excluding .md extension
   // and returning an array containing slug (the filename) as params for every route
